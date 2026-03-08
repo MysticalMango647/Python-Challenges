959ce0033b852d367953e5f852fa869231ec0515
Yunas Magsi <yunasmagsi@cmail.carleton.ca>
2026-03-08 19:08:24 -0400

    snake game: added local file storage for score keeping

--- Snake_Game/scoreboard.py
+++ Snake_Game/scoreboard.py
@@ -7,7 +7,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt", "r") as highscore_record:
+            self.high_score = int(highscore_record.read())
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -16,6 +17,7 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
+
         self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     #updating/removing to add new scoreboard features
@@ -28,6 +30,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", "w") as highscore_record:
+                highscore_record.write(str(self.score))
         self.score = 0
         self.update_scoreboard()
 
